8060255fd92ff42a4fbf70be087f1a1ca05759aa
Melissavazquezunzueta@gmail.com <melissavazquezunzueta@gmail.com>
2021-06-01 19:26:39 -0600

    Codigo Comentado

--- js/todo.js
+++ js/todo.js
@@ -1,8 +1,11 @@
 
-var menu_btn = document.querySelector("#menu-btn");
-var sidebar = document.querySelector("#sidebar");
-var container = document.querySelector(".main");
-menu_btn.addEventListener("click", () => {
-  sidebar.classList.toggle("active-nav");
-  container.classList.toggle("active-cont");
+//responsividad barra de navegacion
+var menu_btn = document.querySelector("#menu-btn"); //meno de barra
+var sidebar = document.querySelector("#sidebar"); //barra lateral
+var container = document.querySelector(".main"); // contenedor
+//al hacer click se genera la accion del boton de la barra
+menu_btn.addEventListener("click", () => { 
+  sidebar.classList.toggle("active-nav"); //barra de navegacion
+  container.classList.toggle("active-cont"); //contenedor
 });
+
